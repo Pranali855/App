e13309ae403bff43d9b70785e470a995d92a6670
pranali <kokatepranu833@gmail.com>
2025-12-27 18:24:58 +0530

    Delete Taskitem.jsx
    changes added

--- todoapp/src/Taskitem.jsx
+++
@@ -1,8 +0,0 @@
-export default function TaskItem({ text, deleteTask }) {
-  return (
-    <li className="task-item">
-      <span>{text}</span>
-      <button className="del-btn" onClick={deleteTask}>✖</button>
-    </li>
-  );
-}
