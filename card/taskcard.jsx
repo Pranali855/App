749740b0d6cb63df1d1bbb49c96d3597b606c30c
pranali <kokatepranu833@gmail.com>
2025-12-30 15:36:21 +0530

    Delete taskcard.jsx
    basic changes added

--- card/taskcard.jsx
+++
@@ -1,9 +0,0 @@
-
-import {Trash2} from 'lucide-react';
-import "./taskcard.css";
-  function taskCard({task}){
-  return <div classname="task-card">
-    {task}
-    <Trash2 classname="icon-delete"/>
-    </div>
-  }
